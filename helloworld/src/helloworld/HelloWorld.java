64bf3e3bebe14a316f5ecd59518848e2a19ab185
Amanda Ribeiro <aamandaribeirocosta@gmail.com>
2023-03-14 11:53:26 -0300

    Projeto Hello World Java

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String nome = "Amanda";
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("\nMeu nome é " + nome);
+	}
+
+}
